cf13790c45dd2166a7c9e1781265cb2228be5197
Tobibito-dev <tobibito.dev@gmail.com>
2022-12-13 11:37:39 +0100

    fix cached mode

--- irminsul/get_data.py
+++ irminsul/get_data.py
@@ -10,42 +10,40 @@ def get_data_json():
 def get_all_data():
     if get_mode() == Mode.ONLINE:
         return online.get_all_data()
-    elif get_mode() == Mode.LOCAL:
+    elif get_mode() == Mode.LOCAL or get_mode() == Mode.CACHE:
         return local.get_all_data()
 
 
 def get_category_data(category: str):
     if get_mode() == Mode.ONLINE:
         return online.get_category_data(category)
-    elif get_mode() == Mode.LOCAL:
+    elif get_mode() == Mode.LOCAL or get_mode() == Mode.CACHE:
         return local.get_category_data(category)
 
 
 def get_item_data(category: str, item: str):
     if get_mode() == Mode.ONLINE:
         return online.get_item_data(category, item)
-    elif get_mode() == Mode.LOCAL:
+    elif get_mode() == Mode.LOCAL or get_mode() == Mode.CACHE:
         return local.get_item_data(category, item)
 
 
 def get_categories():
     if get_mode() == Mode.ONLINE:
         return online.get_categories()
-    elif get_mode() == Mode.LOCAL:
+    elif get_mode() == Mode.LOCAL or get_mode() == Mode.CACHE:
         return local.get_categories()
 
 
 def get_item_names(category: str):
     if get_mode() == Mode.ONLINE:
         return online.get_item_names(category)
-    elif get_mode() == Mode.LOCAL:
+    elif get_mode() == Mode.LOCAL or get_mode() == Mode.CACHE:
         return local.get_item_names(category)
 
 
 def get_item_name(category: str, item: str):
     if get_mode() == Mode.ONLINE:
         return online.get_item_name(category, item)
-    elif get_mode() == Mode.LOCAL:
+    elif get_mode() == Mode.LOCAL or get_mode() == Mode.CACHE:
         return local.get_item_name(category, item)
-
-
